8badc1197b97ad0f6a40a0a44741d877d4657c24
Àlex Barba <alexander.barba@gmail.com>
2019-03-28 14:17:36 +0100

    fix(barbass/searchBar): functions removed

--- barbassStudio/components/barbass/searchBar/src/index.js
+++ barbassStudio/components/barbass/searchBar/src/index.js
@@ -1,11 +1,8 @@
-import React, {useState} from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 import AtomButton from '@schibstedspain/sui-atom-button'
 import AtomInput from '@s-ui/react-atom-input'
 
-function BarbasSearchBar({onQuerySubmit}) {
-  const [query, setQuery] = useState(null)
-
+function BarbasSearchBar() {
   return (
     <div className="sktest-BarbasSearchBar">
       <form className="sktest-BarbasSearchBar-wrapper">
@@ -15,19 +12,8 @@ function BarbasSearchBar({onQuerySubmit}) {
           size="m"
           type="text"
           placeholder="Type in your query!"
-          onChange={e => {
-            e.preventDefault()
-            setQuery(e.target.value)
-          }}
         />
-        <AtomButton
-          className="sktest-BarbasSearchBar-button"
-          onSubmit={e => {
-            e.preventDefault()
-            e.stopPropagation()
-            onQuerySubmit(query)
-          }}
-        >
+        <AtomButton className="sktest-BarbasSearchBar-button">
           Search
         </AtomButton>
       </form>
@@ -39,9 +25,6 @@ BarbasSearchBar.displayName = 'BarbasSearchBar'
 
 // Remove these comments if you need
 // BarbasSearchBar.contextTypes = {i18n: PropTypes.object}
-BarbasSearchBar.propTypes = {
-  onQuerySubmit: PropTypes.function
-}
 //  BarbasSearchBar.defaultProps = {}
 
 export default BarbasSearchBar
